b964d9bae0e8cba7a3b9f211896bec70536f30e3
Junzhou Chen <Junzhou.Chen@med.usc.edu>
2025-11-03 20:19:20 -0800

    Fix issues with d_fwstart not defines.

--- src/cuda/2d/cufinufft2d.cu
+++ src/cuda/2d/cufinufft2d.cu
@@ -317,6 +317,7 @@ int cufinufft2d2_extract(cuda_complex<T> *d_c,
     int blksize = min(d_plan->ntransf - i * d_plan->batchsize, d_plan->batchsize);
     d_cstart    = d_c + i * d_plan->batchsize * d_plan->M;
     d_fkstart   = d_fk + i * d_plan->batchsize * d_plan->ms * d_plan->mt;
+    d_fwstart   = d_fw + i * d_plan->batchsize * d_plan->nf1 * d_plan->nf2;
 
     d_plan->c  = d_cstart;
     d_plan->fw = d_fwstart;
--- src/cuda/3d/cufinufft3d.cu
+++ src/cuda/3d/cufinufft3d.cu
@@ -318,6 +318,7 @@ int cufinufft3d2_extract(cuda_complex<T> *d_c,
     int blksize = min(d_plan->ntransf - i * d_plan->batchsize, d_plan->batchsize);
     d_cstart    = d_c + i * d_plan->batchsize * d_plan->M;
     d_fkstart   = d_fk + i * d_plan->batchsize * d_plan->ms * d_plan->mt * d_plan->mu;
+    d_fwstart   = d_fw + i * d_plan->batchsize * d_plan->nf1 * d_plan->nf2 * d_plan->nf3;
 
     d_plan->c  = d_cstart;
     d_plan->fw = d_fwstart;
